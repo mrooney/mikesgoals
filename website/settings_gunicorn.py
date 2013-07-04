c4c184abb9f5b6b90896b093c4f8f237a1f4e100
Michael Rooney <mrooney.github@rowk.com>
2013-07-04 15:17:56 -0400

    who needs this silly logic

--- website/settings_gunicorn.py
+++ website/settings_gunicorn.py
@@ -1,21 +1,4 @@
-import atexit
-import os
-
 bind = "127.0.0.1:31511"
 pidfile = "./run/gunicorn.pid"
 errorlog = "./run/gunicorn.log"
 workers = 2
-
-def get_port_filename():
-    pid = os.getpid()
-    return os.path.join(os.path.dirname(pidfile), "%s.port" % pid)
-
-def when_ready(server):
-    host, port = server.LISTENER.sock.getsockname()
-    port_filename = get_port_filename()
-    with file(port_filename, 'w') as portfile:
-        print >>portfile, port
-
-    @atexit.register
-    def remove_portfile():
-        os.unlink(port_filename)
